86cd55fd0801b85fd1f74764c18ed3506abbfc28
D. T. Gamage <dahamsat@gmail.com>
2026-07-24 06:00:55 +0530

    Add header file for report-related functions

---
+++ reports.h
@@ -0,0 +1,10 @@
+#ifndef REPORTS_H
+#define REPORTS_H
+
+void viewRevenueReport(void);
+void clearInputBuffer(void);
+void pauseScreen(void);
+int getValidatedInt(int min, int max);
+int stringCompareIgnoreCase(const char *s1, const char *s2);
+
+#endif
